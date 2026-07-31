be9c278e05cef9f0b1951fa67ad6cf1207e058ab
stephenkosisochukwu9-byte <stephenkosisochukwu9@gmail.com>
2026-07-31 14:47:02 +0100

    resize product size

--- components/products/Products.tsx
+++ components/products/Products.tsx
@@ -135,7 +135,7 @@ const { search, setSearch } = useSearch();
 
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
 
         {filteredProducts.map((product) => (
            
