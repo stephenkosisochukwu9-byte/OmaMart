14fbe2975e360ccfd3d36053a8fe4cdde7a2e049
stephenkosisochukwu9-byte <stephenkosisochukwu9@gmail.com>
2026-08-03 15:42:31 +0100

    resizing the texts

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 
 export default function Banner() {
   return (
-    <section className="max-w-7xl mx-auto px-6 py-10">
+   <section className="max-w-7xl mx-auto px-4 md:px-6 py-4 md:py-10">
       <div className="bg-gradient-to-r from-orange-500 to-orange-600 rounded-3xl overflow-hidden">
 
         <div className="flex flex-col-reverse md:grid md:grid-cols-2 items-center">
           {/* Left Side */}
-          <div className="p-6 md:p-10 text-white">
+          <div className="px-5 py-5 md:p-10 text-white">
             <span className="bg-white text-orange-600 px-4 py-2 rounded-full text-sm font-semibold">
               Limited Time Offer
             </span>
 
-            <h2 className="text-3xl md:text-5xl font-bold mt-4 md:mt-6 leading-tight">
+           <h2 className="text-2xl md:text-5xl font-bold mt-2 leading-tight">
               Weekend Mega Sale
             </h2>
 
-            <p className="mt-4 text-base md:text-xl">
+            <p className="mt-2 text-sm md:text-xl leading-6">
               Save up to{" "}
               <span className="text-yellow-300 font-bold">
                 50% OFF
@@ -24,19 +24,19 @@ export default function Banner() {
               on home appliances, kitchen essentials and household products.
             </p>
 
-           <button className="mt-6 bg-white text-orange-600 px-6 py-3 md:px-8 md:py-4 rounded-full font-bold hover:bg-gray-100 transition">
+           <button className="mt-4 bg-white text-orange-600 px-6 py-3 md:px-8 md:py-4 rounded-full font-bold hover:bg-gray-100 transition">
               Shop Deals
             </button>
           </div>
 
           {/* Right Side */}
-          <div className="flex justify-center p-6">
+          <div className="flex justify-center p-4 md:p-6">
             <Image
               src="/images/banner/banner.png"
               alt="Weekend Mega Sale"
               width={700}
               height={350}
-              className="w-full max-w-2xl object-contain"
+              className="w-full max-w-md md:max-w-2xl object-contain"
             />
           </div>
 
